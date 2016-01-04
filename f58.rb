9ef046e2dba0d473ece006c62500eb675f053575
muzik <cgicn@qq.com>
2016-01-04 19:29:25 +0800

    f58

--- f58.rb
+++ f58.rb
@@ -3,29 +3,46 @@ require 'anemone'
 class F58
   def run
     cities = fetch_cities 
-    seeds = cities.collect{|r| r[:url] + "ershoufang/"}
-    seeds = seeds[0,1]
+    seeds = cities.collect{|r| r[1][:url] + "ershoufang/"}
+    # seeds = seeds[0,1]
     Anemone.crawl seeds,verbose: true do |a|
       a.focus_crawl do |page|
         []
       end
       a.on_every_page do |page|
+        subdomain = page.url.to_s.match(/\/\/(.+?)\./)[1]
+        parent = cities[subdomain]
+        parent[:children] = [] unless parent.key? :children
         page.doc.at_css('#region.secitem').css('dd a').each do |node|
           next unless node.attr('class').nil?
           next unless !node.attr('onclick').nil? and node.attr('onclick').match(/_area_/)
-          p node.content.strip
+          data = {}
+          data[:name] = node.content.strip
+          data[:slug] = node.attr('href').match(/^\/(.+?)\//)[1]
+          parent[:children] << data
         end
+        cities[subdomain] = parent
+      end
+      a.after_crawl do
+        dump_to_file cities
       end
     end
   end
   def fetch_cities
     url = 'http://www.58.com/changecity.aspx'
     page = Anemone::HTTP.new.fetch_page url
-    cities = []
+    cities = {}
     page.doc.css('#clist dd a').each do |node|
-      cities <<  {name: node.content,url: node.attr('href')}
+      url = node.attr('href')
+      slug = url.match(/\/\/(.+?)\./)[1]
+      cities[slug] =  {name: node.content,url: url,slug: slug}
     end
     cities
+  end
+  def dump_to_file data,file='f58.yml'
+    File.open(file,'w') { |f|
+      f.write data.deep_stringify_keys.to_yaml
+    }
   end
 end
 F58.new.run
